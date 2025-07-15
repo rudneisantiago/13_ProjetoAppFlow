67fc0ec226787a6e799dd48b9f187f21a1db31d6
Rudnei Santiago <rudnei.santiago@gmail.com>
2025-07-15 16:20:19 -0300

    Organizando rotas e métodos http

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,20 @@ import {
   getFilterEpisodes,
   getListEpisodes,
 } from "./controllers/podcasts-controller";
+import { Routes } from "./routes/routes";
+import { HttpMethods } from "./utils/http-methods";
 
 async function main() {
   const server = createServer(
     async (req: IncomingMessage, res: ServerResponse) => {
       const url = new URL(`${process.env.HOST}${req.url}`);
+      const { pathname } = url;
 
-      if (req.method === "GET" && url.pathname === "/api/list") {
+      if (req.method === HttpMethods.GET && pathname === Routes.LIST) {
         await getListEpisodes(req, res);
       }
 
-      if (req.method === "GET" && url.pathname === "/api/episode") {
+      if (req.method === HttpMethods.GET && pathname === Routes.EPISODE) {
         await getFilterEpisodes(req, res);
       }
     }
---
+++ src/routes/routes.ts
@@ -0,0 +1,6 @@
+enum Routes {
+  LIST = "/api/list",
+  EPISODE = "/api/podcasts",
+}
+
+export { Routes };
