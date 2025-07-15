edd995f569d39457cdfa716f1252283866fd6975
Rudnei Santiago <rudnei.santiago@gmail.com>
2025-07-15 15:45:19 -0300

    Transferindo lógica do controller pro service

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -3,16 +3,14 @@ import { serviceListEpisodes } from "../services/list-episodes-service";
 import { serviceFilterEpisodes } from "../services/filter-episodes-service";
 
 const getListEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
-  res.writeHead(200, { "content-type": "application/json" });
   const content = await serviceListEpisodes();
+
+  res.writeHead(200, { "content-type": "application/json" });
   res.end(JSON.stringify(content));
 };
 
 const getFilterEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
-  const url = new URL(`${process.env.HOST}${req.url}`);
-  const params = url.searchParams.get("p");
-
-  const content = await serviceFilterEpisodes(params ?? "");
+  const content = await serviceFilterEpisodes(req);
 
   res.writeHead(200, { "content-type": "application/json" });
   res.end(JSON.stringify(content));
--- src/services/filter-episodes-service.ts
+++ src/services/filter-episodes-service.ts
@@ -1,9 +1,13 @@
+import { IncomingMessage } from "http";
 import { PodcastModel } from "../models/podcast-model";
 import { repositoryPodcast } from "../repositories/podcasts-repository";
 
 const serviceFilterEpisodes = async (
-  podcastName: string
+  req: IncomingMessage
 ): Promise<PodcastModel[]> => {
+  const url = new URL(`${process.env.HOST}${req.url}`);
+  const podcastName = url.searchParams.get("p") ?? "";
+
   const data = await repositoryPodcast(podcastName);
 
   return data;
